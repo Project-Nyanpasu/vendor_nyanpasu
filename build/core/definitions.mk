6efb66c12995191a04019a04577cfd6f965ef313
Steve Kondik <steve@cyngn.com>
2015-04-05 10:59:21 -0400

    nyanpasu: Nyanpasu-specific build macros
     * vendor/*/build/core/definitions.mk is automatically sourced by the
       build system to load custom macros. Start using this for some
       of our own.
     * Adds a "uniq" macro to remove duplicates from a list without changing
       it's order.
    
    Change-Id: Id5f1eb4b9f81bb31ca0b3d5e74c298b3d105da10

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,3 +1,10 @@
+#
+# Nyanpasu-specific macros
+#
+define uniq
+$(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+endef
+
 # Include board/platform macros
 include vendor/nyanpasu/build/core/utils.mk
 
