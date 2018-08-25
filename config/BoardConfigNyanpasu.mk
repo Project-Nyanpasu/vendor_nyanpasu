189644d1c17a673eaf442519ce9f821e791ac11f
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    nyanpasu: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- config/BoardConfigNyanpasu.mk
+++ config/BoardConfigNyanpasu.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Qcom-specific bits
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/nyanpasu/config/BoardConfigQcom.mk
+endif
+
 # Kernel
 include vendor/nyanpasu/config/BoardConfigKernel.mk
 
