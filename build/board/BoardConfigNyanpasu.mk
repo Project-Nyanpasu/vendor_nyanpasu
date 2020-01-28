5fc98b7a05d3e074bf9c987a47f152db877695ca
Jake Weinstein <jake@aospa.co>
2020-01-28 05:45:40 +0100

    nyanpasu: Disable EAP Proxy
    EAP Proxy depends on proprietary headers and breaks WPA Supplicant compilation.
    
    Change-Id: I0c50cb07c6d064dca008bd452fc97104b7695107

--- build/board/BoardConfigNyanpasu.mk
+++ build/board/BoardConfigNyanpasu.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Disable EAP Proxy because it depends on proprietary headers
+# and breaks WPA Supplicant compilation.
+DISABLE_EAP_PROXY := true
+
 # Kernel
 include vendor/nyanpasu/build/board/BoardConfigKernel.mk
 
