1ca59dab0c0c84f00921965dde370025319a6a25
Christian Oder <myself5@carbonrom.org>
2021-01-05 12:31:37 -0300

    nyanpasu: Add basic hotword permissions
     * Required for older hotword blobs
    
    Change-Id: I3a978ddda791a00274064e47348953c46006ace2

--- build/product/common.mk
+++ build/product/common.mk
@@ -13,6 +13,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 # Permissions
 PRODUCT_COPY_FILES += \
     vendor/nyanpasu/build/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/nyanpasu/build/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/nyanpasu/build/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Inherit
