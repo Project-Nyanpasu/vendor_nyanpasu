Project-1CE/vendor_ice

soong: add config variables for udfps extension
Change-Id: I499b31d00a053b865ceaa3b34a9762f980bbf14e

target/board/BoardConfigSoong.mk
@@ -29,12 +29,15 @@ SOONG_CONFIG_NAMESPACES += iceGlobalVars
 SOONG_CONFIG_iceGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_iceGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_iceGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_iceGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_iceGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)

Project-Nyanpasu/vendor_nyanpasu

build/board/BoardConfigSoong.mk
@@ -19,12 +19,15 @@ SOONG_CONFIG_NAMESPACES += nyanpasuGlobalVars
 SOONG_CONFIG_nyanpasuGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_nyanpasuGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_nyanpasuGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_nyanpasuGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_nyanpasuGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)